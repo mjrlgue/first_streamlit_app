f861100083bf5c5d48b82b3225033a43238e2a0e
Marwane Chahoud <marwane.chahoud@gmail.com>
2024-02-09 17:27:36 +0100

    Update streamlit_app.py
    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title("My parent new dinner")
-streamlit.header("Breakfast Menu")
-streamlit.text ("Omega 3 and blue berry")
-streamlit.text ("kale, spinach & rocket smoothie")
-streamlit.text ("Juice")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
