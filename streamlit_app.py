a339380a2ccd4a330060edeb3639e4a19ed1c905
Marwane Chahoud <marwane.chahoud@gmail.com>
2024-02-09 17:29:18 +0100

    Update streamlit_app.py
    add another header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit
 
 streamlit.title("My parent new dinner")
+
 streamlit.header('🍞Breakfast Menu')
 streamlit.text('🥣 🐔 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
